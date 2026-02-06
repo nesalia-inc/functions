85f499c8f5a8753b3df98bfce9dd582a00d06c22
AliiiBenn <davidddpereiraaa6@gmail.com>
2026-02-06 13:19:08 +0100

    Update

--- web/src/mdx-components.tsx
+++ web/src/mdx-components.tsx
@@ -1,12 +1,9 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-import { Tabs, Tab } from 'fumadocs-ui/components/tabs';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    Tabs,
-    Tab,
     ...components,
   };
 }
